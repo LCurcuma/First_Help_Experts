a5c8ff856647fc500fa3e351975d7d448ca1432b
holze <holze.hansen@gmailc.com>
2026-05-18 09:27:41 +0200

    klar gjorde index.php

--- index.php
+++ index.php
@@ -10,21 +10,47 @@ require "settings/init.php";
 <head>
     <meta charset="utf-8">
     
-    <title>Sigende titel</title>
+    <title>Login - Førstehjælpeksperten</title>
     
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
-    
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
-    
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Stylesheet -->
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <!-- Font Awesome ikoner -->
+    <script src="https://kit.fontawesome.com/737b386bab.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstraps ikoner -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- AOS - Animate On Scroll Library -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <!-- Favicon: https://favicon.io/favicon-converter/ -->
+    <link rel="apple-touch-icon" sizes="180x180" href="img/logo/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/logo/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/logo/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+
 </head>
 
 <body>
 
-<h1>Det virker 🥳</h1>
+<h1>Login side</h1>
 
+<!------------ Bootstrap library ------------>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<!------------ AOS LIBRARY ------------>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+
+
 </body>
 </html>
