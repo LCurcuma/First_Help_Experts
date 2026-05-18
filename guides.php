1689927099f9bcdca12e657ad7d1f871a948a309
piape <pip002@edu.zealand.dk>
2026-05-18 09:49:31 +0200

    guids fil tilføjet

--- guides.php
+++ guides.php
@@ -1,1 +1,47 @@
 <?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Login - Førstehjælpeksperten</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Stylesheet -->
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <!-- Font Awesome ikoner -->
+    <script src="https://kit.fontawesome.com/737b386bab.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstraps ikoner -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+
+    <!-- Favicon: https://favicon.io/favicon-converter/ -->
+    <link rel="apple-touch-icon" sizes="180x180" href="img/logo/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/logo/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/logo/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+
+</head>
+
+<body>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
