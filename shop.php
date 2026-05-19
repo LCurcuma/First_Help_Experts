62171fe05d5da5701f4eec293f6d5ea483773286
LCurcuma <zhihnadya2002@gmail.com>
2026-05-19 09:54:01 +0200

    tilføjet desktop version af user.php til shop.php

--- shop.php
+++ shop.php
@@ -70,6 +70,51 @@ foreach ($data as $card) {
         } ?>
         </div>
     </div>
+    <div class="d-none flex-column d-lg-block">
+        <div class="container">
+            <div class="row">
+                <div class="col col-7"></div>
+
+                <!--USER SECTION DESKTOP, SOM KAN GENNEMBRUGES-->
+                <div class="col col-5 user_section">
+                    <div class="top_container">
+                        <!--the container with points-->
+                        <?php include "components/money_container.php" ?>
+                    </div>
+                    <!--the avatar container-->
+                    <?php include "components/avatar.php" ?>
+                    <!--the score container-->
+                    <?php include "components/score_container_user.php" ?>
+                    <!--container med links-->
+                    <section class="links_section">
+                        <a href="" class="link_tile" style="background: #CDB4D1;" onclick="alert('Funktion kommer snart')">
+                            <h2 class="h2_bold">Check ind</h2>
+                            <div class="plus_money">
+                                <h2>+5</h2>
+                                <img src="img/icons/3d-icons/money.png" class="money_plus_image" alt="Points">
+                            </div>
+                        </a>
+                        <a href="shop.php" class="link_tile" style="background: #88DB95;">
+                            <div>
+                                <h2 class="h2_bold">Point shop</h2>
+                                <p>350 tilgængelige rewards</p>
+                            </div>
+                            <img src="img/icons/3d-icons/money.png" class="money_image_big" alt="Points">
+                        </a>
+                        <a href="" class="link_tile" style="background: #F5A623;" onclick="alert('Funktion kommer snart')">
+                            <div>
+                                <h2 class="h2_bold">Din førstehjælpsbevis</h2>
+                                <p>Gyldig til <span>12.04.2026</span></p>
+                            </div>
+                            <img src="img/icons/3d-icons/checkmark2.png" class="money_image_big" alt="Check">
+                        </a>
+                    </section>
+
+                    <a href="index.php" class="logout">Log ud</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!------------ Bootstrap library ------------>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
