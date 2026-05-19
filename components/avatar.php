73cde97c5e9ca2a1913dd21a0a58583f3d630382
LCurcuma <zhihnadya2002@gmail.com>
2026-05-19 09:24:34 +0200

    tilføjet til settings-knap "funktion kommer snart"

--- components/avatar.php
+++ components/avatar.php
@@ -1,7 +1,7 @@
 
     <div class="avatar_container">
         <img src="../img/icons/3d-icons/avater.png" class="avatar" alt="Avatar"/>
-        <a class="settings_button" href="../settings.php">
+        <a class="settings_button" href="" onclick="alert('Funktion kommer snart')">
             <i class="fa-solid fa-gear"></i>
         </a>
     </div>
