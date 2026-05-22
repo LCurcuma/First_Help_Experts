727528c296d785b4c9c19887386d7182ab2da265
piape <pip002@edu.zealand.dk>
2026-05-23 00:58:34 +0200

    tilføjet variablen som henter data fra json og sender vider til guide-skabelon

--- blødning-guide.php
+++ blødning-guide.php
@@ -2,13 +2,8 @@
 
 require "settings/init.php";
 
-// Det id PHP skal lede efter i JSON (Skal matche præcis det, der står før kolon i JSON)
 $guideId = "blødning";
-
-// Den store overskrift øverst på app-skærmen
 $guideTitel = "Blødning";
-
-// Navnene på de runde badges øverst. Da du har 5 trin i din JSON, skal der være 5 badges:
 $badgeNavne = ["Ro", "Stop", "Løft", "Forbind", "Alarm"];
 
 include "guide-skabelon.php";
