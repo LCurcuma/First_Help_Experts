206b285dbd50a9c0f6bd447cc8e7e665b5bf7c33
piape <pip002@edu.zealand.dk>
2026-05-23 01:07:13 +0200

    fjernet mellemrum

--- bevidstløs-guide.php
+++ bevidstløs-guide.php
@@ -2,11 +2,9 @@
 
 require "settings/init.php";
 
-// Fortæl din fælles skabelon, at den skal trække HLR-dataen ud af din JSON
 $guideId = "bevidstløshed";
 $guideTitel = "Bevidstløshed";
 $badgeNavne = ["Tjek", "Ring", "Luftvej", "Sideleje", "HLR"];
 
-// Hent skabelon-filen, som bygger hele siden automatisk
 include "guide-skabelon.php";
 ?>
--- brandsår-guide.php
+++ brandsår-guide.php
@@ -2,11 +2,9 @@
 
 require "settings/init.php";
 
-// Fortæl din fælles skabelon, at den skal trække HLR-dataen ud af din JSON
 $guideId = "brandsår";
 $guideTitel = "Brandsår";
 $badgeNavne = ["Ro", "Køl", "Fjern", "Dæk", "Varm", "Alarm"];
 
-// Hent skabelon-filen, som bygger hele siden automatisk
 include "guide-skabelon.php";
 ?>
