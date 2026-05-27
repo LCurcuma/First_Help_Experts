b07ea19707ac01a4f85e569e4ecf64a1d208f534
holze <holze.hansen@gmailc.com>
2026-05-27 17:52:39 +0200

    Ændrede user section kode, til et component på forside.php

--- forside.php
+++ forside.php
@@ -158,7 +158,7 @@ $nameArray = explode(" ", $userData[0]->name);
 
 
 <!-------------- DESKTOP LAYOUT ------------->
-<div class="d-none d-lg-block container mt-5">
+<div class="d-none d-lg-block container mt-5" >
 
     <!-- Desktop grid layout -->
     <div class="row g-5">
@@ -253,49 +253,13 @@ $nameArray = explode(" ", $userData[0]->name);
 
         </div>
 
-        <!--USER SECTION DESKTOP, SOM KAN GENNEMBRUGES-->
+        <!-- USER SECTION.PHP COMPONENTS -->
         <div class="col col-4 user_section">
-            <div class="top_container">
-                <!--the container with points-->
-                <?php include "components/money_container.php" ?>
-            </div>
-            <!--the avatar container-->
-            <?php include "components/avatar.php" ?>
-            <!--the score container-->
-            <?php include "components/score_container_user.php" ?>
-            <!--container med links-->
-            <section class="links_section_desk">
-                <a href="#" class="link_tile_desk" style="background: #826099;" onclick="alert('Funktion kommer snart')">
-                    <h2 class="h2_bold">Check ind</h2>
-                    <div class="plus_money_desk">
-                        <h2>+5</h2>
-                        <img src="img/icons/3d-icons/money.png" class="money_plus_image_desk" alt="Points">
-                    </div>
-                </a>
-                <a href="shop.php?id=<?php echo $id?>" class="link_tile_desk" style="background: #88DB95;">
-                    <div>
-                        <h2 class="h2_bold">Point shop</h2>
-                        <p>350 tilgængelige rewards</p>
-                    </div>
-                    <img src="img/icons/3d-icons/money.png" class="money_image_big" alt="Points">
-                </a>
-                <a href="#" class="link_tile_desk" style="background: linear-gradient(180deg, #F5A623 0%, #DE951C 100%);" onclick="alert('Funktion kommer snart')">
-                    <div>
-                        <h2 class="h2_bold">Din førstehjælps<br/>bevis</h2>
-                        <p>Gyldig til <span>12.04.2026</span></p>
-                    </div>
-                    <img src="img/icons/3d-icons/checkmark2.png" class="money_image_big" alt="Check">
-                </a>
-            </section>
-
-            <a href="index.php" class="logout">Log ud</a>
+            <?php include "components/user_section.php"; ?>
         </div>
-        <!--USER SECTION DESKTOP, SOM KAN GENNEMBRUGES-->
 
-
-
-
-
+    </div>
+</div>
 <!-------------- DESKTOP LAYOUT ------------->
 
 
