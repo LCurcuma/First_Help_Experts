ec4c569957c8f0f879ff54549ad975294dc12bda
holze <holze.hansen@gmailc.com>
2026-05-27 18:01:39 +0200

    Ændrede missioner fra 5 til 8 på og gjorde hele cardet klickbart weekly-missions-card2-desktop.php.php

--- components/weekly-missions-card2-desktop.php
+++ components/weekly-missions-card2-desktop.php
@@ -1,11 +1,9 @@
-<div class="weekly-missions-card fp-action-card position-relative overflow-hidden">
+<a href="quiz.php?id=1" class="weekly-missions-card fp-action-card position-relative overflow-hidden d-block text-decoration-none text-white">
 
     <img src="img/ui/circle1.svg" class="weekly-circle weekly-c1" alt="Baggrunds element">
     <img src="img/ui/circle1.svg" class="weekly-circle weekly-c2" alt="Baggrunds element">
     <img src="img/ui/circle1.svg" class="weekly-circle weekly-c3" alt="Baggrunds element">
     <img src="img/ui/circle1.svg" class="weekly-circle weekly-c4" alt="Baggrunds element">
-
-    <a href="quiz.php?id=1" class="stretched-link"></a>
 
     <div class="d-flex flex-column justify-content-between h-100 position-relative z-2 w-75">
 
@@ -26,9 +24,9 @@
 
             <div class="weekly-missions-progress-bg position-relative rounded-5">
 
-                <div class="weekly-missions-progress-bar rounded-5" style="width: <?php echo $userData[0]->finished_weekly_missions/5*100?>%;"></div>
+                <div class="weekly-missions-progress-bar rounded-5" style="width: <?php echo $userData[0]->finished_weekly_missions/8*100?>%;"></div>
 
-                <span class="weekly-missions-progress-text fw-light small opacity-50"><?php echo ($userData[0]->finished_weekly_missions / 5) * 100; ?>%</span>
+                <span class="weekly-missions-progress-text fw-light small opacity-50"><?php echo ($userData[0]->finished_weekly_missions / 8) * 100; ?>%</span>
 
             </div>
 
@@ -38,4 +36,4 @@
 
     <img src="img/icons/3d-icons/arrow.png" alt="Mål ikon" class="fp-card-icon">
 
-</div>
+</a>
