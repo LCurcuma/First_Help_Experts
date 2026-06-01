4542545f97bf4d51af1ce417d3e857918bc73691
LCurcuma <zhihnadya2002@gmail.com>
2026-06-01 20:41:33 +0200

    tilføjet kommentarer på money_container

--- components/money_container.php
+++ components/money_container.php
@@ -1,4 +1,7 @@
+<!-- Container med penge som bruges på mobile version af shop -->
 <div class="money_container">
+<!-- Points -->
     <p class="money_count" id="points_desk"><?php echo $userData[0]->points?></p>
+    <!-- Billede af penge -->
     <img src="img/icons/3d-icons/money.png" class="money_image" alt="Points"/>
 </div>
