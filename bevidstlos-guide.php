793462760645205e6abac3dc0c730ccf8a7d7794
piape <pip002@edu.zealand.dk>
2026-06-01 23:15:23 +0200

    justering

--- bevidstlos-guide.php
+++ bevidstlos-guide.php
@@ -3,7 +3,7 @@
 //inklusions-funktion, der indlæser systemets opstartsfil (f.eks. databaseforbindelse og globale indstillinger). Hvis filen mangler, stopper scriptet helt (require), hvilket sikrer mod systemfejl.
 require "settings/init.php";
 
-//En variabel (streng), der definerer en unik ID-nøgle for denne guide. Den bruges af dit JavaScript til at finde de helt rigtige trin inde i min JSON-datafil.
+//En variabel (streng), der definerer en unik ID-nøgle for denne guide. Den bruges af mit JavaScript til at finde de helt rigtige trin inde i min JSON-datafil.
 $guideId = "bevidstloshed";
 
 //variabel (streng), der gemmer den officielle overskrift. Den sendes med over i skabelonen, så der står "Bevidstløshed" i toppen af skærmen på mobilen.
